60b94a880a788f71dbd2069b08122ebfe8047179
malhotrankit <ankit18071994@gmail.com>
2021-08-15 19:27:22 +0200

    add currency-conversion-service

---
+++ currency-conversion-service/src/main/java/com/ankit/microservices/currencyConversionService/CurrencyConversionServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ankit.microservices.currencyConversionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients("com.ankit.microservices.currencyConversionService.controller")
+@SpringBootApplication
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
